72f051e0ab380f7c3c427c5971bc87f6501096ba
MatasKornelijusVanagas <matas.vanagas@knf.stud.vu.lt>
2024-02-12 16:30:31 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+kintamasis = 1
+kintamasis = 2
+
+def funkcija():
+    print(kintamasis)
+    print(kintamasis2)
+    print(F'test:{kintamasis2}{kintamasis}')
+    print('test: {}{}'.format(kintamasis2, kintamasis))
+
+
+
