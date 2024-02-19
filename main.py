d5782190c438427a20218494777bfdc40631f6bd
MatasKornelijusVanagas <159789674+MatasKornelijusVanagas@users.noreply.github.com>
2024-02-19 20:50:58 +0200

    Update main.py
    Padariau skaičiuotuvo meniu, žinau šiek tiek vėluoju, nes pamiršau tikrai. Ką atlikau, tai 41 eilutėje pasidariau new line, kad terminale būtų patogiau skaityti, nuo 42 iki 47 surašiau galimus veiksmų pasirinkimus, 49 eilutėje yra įvedamas pasirinkimas veiksmui atlikti, 51 eilut4je naudoju if funkciją, jei pasirinkimas tenkina visus galimus variantus - tada galima įvesti pirmą ir antrą skaičių 52 ir 53 eilutėje. Aišku, aš galėjau tiesiog tuos kintamuosius įrašyti 15 ir 16 eilutėje, bet jie visada vienodi būtų. Juos taip pat float palikau, kad po kablelio būtų, bet paskui žiūriu, jog anuos kintamuosius irgi rodo terminale po kablelio ir be float, tai nelabai supratau ar būtina juos naudot. Nuo 55 iki 64 išvedami unikalus kiekvieno pasirinkimo veiksmų rezultatai.

--- main.py
+++ main.py
@@ -36,3 +36,34 @@ print("atimtis:", atimtis(pirmas, antras))
 print("daugyba:", daugyba(pirmas, antras))
 print("dalyba:", dalyba(pirmas, antras))
 print("pakelta:", pakeltilaipsniu(pirmas, antras))
+
+while True:
+    print("\nMatematiniai veiksmai:")
+    print("1. Sudėtis")
+    print("2. Atimtis")
+    print("3. Daugyba")
+    print("4. Dalyba")
+    print("5. Pakelti laipsniu")
+    print("0. Baigti darbą")
+    
+    pasirinkimas = input("Įvesti operaciją (1/2/3/4/5/0): ")
+    
+    if pasirinkimas in ['1', '2', '3', '4', '5']:
+        pirmas_skaicius = float(input("Įvesti pirmą skaičių: "))
+        antras_skaicius = float(input("Įvesti antrą skaičių: "))
+        
+        if pasirinkimas == '1':
+            print("Rezultatas:", sudetis(pirmas_skaicius, antras_skaicius))
+        elif pasirinkimas == '2':
+            print("Rezultatas:", atimtis(pirmas_skaicius, antras_skaicius))
+        elif pasirinkimas == '3':
+            print("Rezultatas:", daugyba(pirmas_skaicius, antras_skaicius))
+        elif pasirinkimas == '4':
+            print("Rezultatas:", dalyba(pirmas_skaicius, antras_skaicius))
+        elif pasirinkimas == '5':
+            print("Rezultatas:", pakeltilaipsniu(pirmas_skaicius, antras_skaicius))
+    elif pasirinkimas == '0':
+        print("Programos darbas atliktas.")
+        break
+    else:
+        print("Nėra tokio pasirinkimo. Bandyti dar kartą.")
